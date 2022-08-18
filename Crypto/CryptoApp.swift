f81346abf5a56cdcd87b420fd800191ff71f6d99
CSH <calum.haddow11@gmail.com>
2022-08-18 19:50:49 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Calum Haddow on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
